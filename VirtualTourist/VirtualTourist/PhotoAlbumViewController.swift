e1e114c9e38417a62b17b04bab5585468864f0f2
Jonathan Grubb <geekygamer@gmail.com>
2016-05-30 15:31:01 -0500

    controllers and their classes created

---
+++ VirtualTourist/VirtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAlbumViewController.swift
+//  VirtualTourist
+//
+//  Created by Jonathan Grubb on 5/30/16.
+//  Copyright © 2016 Jonathan Grubb. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TravelLocationsMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Jonathan Grubb on 5/30/16.
@@ -8,18 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class TravelLocationsMapViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
 
