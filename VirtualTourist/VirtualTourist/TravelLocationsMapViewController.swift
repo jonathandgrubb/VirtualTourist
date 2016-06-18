ff8fc20575649da4970911b04b16d4251e20c69e
Jonathan Grubb <geekygamer@gmail.com>
2016-06-18 15:33:43 -0500

    was able to pin two locations and see photos from each one. they will no load the first time the Photos view is listed and the pin click isn't as responsive as before (must click something other than the pin, then click the pin, if the event is going to fire)

--- VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
+++ VirtualTourist/VirtualTourist/TravelLocationsMapViewController.swift
@@ -180,20 +180,30 @@ extension TravelLocationsMapViewController: MKMapViewDelegate {
             
             print("annotation (\(lat),\(lon)) was selected")
 
-            /* create fetch request for the selected pin
+            // create fetch request for the selected pin
             let fr = NSFetchRequest(entityName: "Pin")
             fr.sortDescriptors = []
-            let pred = NSPredicate(format: "latitude = %@ AND longitude = %@", annotation.coordinate.latitude, annotation.coordinate.longitude)
+            let pred = NSPredicate(format: "(latitude = %@) AND (longitude = %@)", lat, lon)
             fr.predicate = pred
             executeSearch()
-            if let pin = fetchedResultsController!.fetchedObjects?.first as? Pin {
+            /*if let pin = fetchedResultsController!.fetchedObjects?.first as? Pin {
                 selectedPin = pin
                 print("pin was fetched: \(pin)")
                 // prepare to open the pictures associated with the place just clicked
                 performSegueWithIdentifier("PhotosViewSegue", sender: nil)
+            }*/
+            if let pins = fetchedResultsController!.fetchedObjects as? [Pin] {
+                let filteredPins = pins.filter { (p: Pin) -> Bool in
+                    return p.latitude == lat && p.longitude == lon
+                }
+                if let pin = filteredPins.first {
+                    selectedPin = pin
+                    print("pin was fetched: \(pin.latitude),\(pin.longitude)")
+                    // prepare to open the pictures associated with the place just clicked
+                    performSegueWithIdentifier("PhotosViewSegue", sender: nil)
+                }
             }
-            */
-            performSegueWithIdentifier("PhotosViewSegue", sender: nil)
+            //performSegueWithIdentifier("PhotosViewSegue", sender: nil)
         
         } else {
             print("error clicking annotation")
@@ -209,25 +219,25 @@ extension TravelLocationsMapViewController {
         if segue.identifier! == "PhotosViewSegue" {
             
             if let photosVC = segue.destinationViewController as? PhotoAlbumViewController,
-               let annotation = mapView.selectedAnnotations.first {
-               //let pin = selectedPin {
+               let annotation = mapView.selectedAnnotations.first,
+               let pin = selectedPin {
                 
-                /* create fetch request for the selected photos for the pin
+                // create fetch request for the selected photos for the pin
                 let fr = NSFetchRequest(entityName: "Photo")
                 fr.sortDescriptors = []
                 let pred = NSPredicate(format: "pin = %@", pin)
                 fr.predicate = pred
-                */
                 
-                // create fetch request for the selected pin and try to get the photos by the linked relationship
+                /* create fetch request for the selected pin and try to get the photos by the linked relationship
                 let fr = NSFetchRequest(entityName: "Photo")
                 fr.sortDescriptors = []
                 let pred = NSPredicate(format: "pin.latitude = %@ AND pin.longitude = %@", annotation.coordinate.latitude, annotation.coordinate.longitude)
                 fr.predicate = pred
+                */
                 
                 let fc = NSFetchedResultsController(fetchRequest: fr, managedObjectContext: fetchedResultsController!.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
                 
-                //photosVC.selectedPin = pin
+                photosVC.selectedPin = pin
                 photosVC.location = annotation
                 photosVC.fetchedResultsController = fc
             }
