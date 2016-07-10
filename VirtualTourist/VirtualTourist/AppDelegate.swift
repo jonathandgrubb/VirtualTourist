e996a055f56e89251c6dd32fa79b621290c1ee0f
Jonathan Grubb <geekygamer@gmail.com>
2016-07-10 17:07:18 -0500

    calling checkIfFirstLaunch() from applicationdidFinishLaunchingWithOptions

--- VirtualTourist/VirtualTourist/AppDelegate.swift
+++ VirtualTourist/VirtualTourist/AppDelegate.swift
@@ -30,6 +30,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        checkIfFirstLaunch()
+        
         // Start Autosaving
         stack.autoSave(60)
         
